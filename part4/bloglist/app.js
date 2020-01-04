ffa85f10d1bd01c39742ce28415f229f81ae63e7
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2020-01-04 03:47:37 -0300

    Update part4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
@@ -20,6 +22,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static('build'))
-app.use('/api', blogsRouter)
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,75 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
 
-blogsRouter.get('/blogs', (request, response) => {
-    Blog
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization
+        && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog
         .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+        .populate('user', { username: 1, name: 1 })
+
+    response.json(notes.map(note => note.toJSON()))
 })
 
-blogsRouter.post('/blogs', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+    const body = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
+    const token = getTokenFrom(request)
+    try {
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+
+        const user = await User.findById(body.userId)
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes,
+            user: user._id
         })
+
+        const savedBlog = await blog.save()
+        user.notes = user.notes.concat(savedNote._id)
+        await user.save()
+        response.json(savedBlog.toJSON())
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+    try {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote.toJSON())
+        })
+        .catch(error => next(error))
 })
 
 module.exports = blogsRouter
